aa949db62ace768d1d5b0e6a4d4d7a969959cfc5
Matt Kuras <mattkuras@gmail.com>
2020-02-24 18:13:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash ={}
+  array = []
+  data.each do |first_level, all_other| 
+    the_rest.each do |category, array|
+      hash[name] << {color: [], gender: [], lives: []}
+      end
+    end
+    binding.pry
 end

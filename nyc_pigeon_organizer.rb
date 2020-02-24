218808f6abaa92998fa25ba1d0b54e569b532c16
Matt Kuras <mattkuras@gmail.com>
2020-02-24 18:53:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash ={}
+  array = []
+  data.each do |first_level, all_other| 
+    all_other.each do |category, array|
+      array.each do |name|
+      hash[name] = {color: [], gender: [], lives: []}
+      end
+    end
+  end
+
+  x = hash.keys
+  data[:color].each do |color, array| 
+    array.each do |bird|
+      x.each do |item|
+        if bird === item
+          hash[item][:color] << color.to_s 
+        end
+    end
+  end
+  end
+  
+  data[:gender].each do |gender, array| 
+    array.each do |bird|
+      x.each do |item|
+        if bird === item
+          hash[item][:gender] << gender.to_s 
+        end
+    end
+  end
+  end
+  
+  data[:lives].each do |location, array| 
+    array.each do |bird|
+      x.each do |item|
+        if bird === item
+          hash[item][:lives] << location 
+        end
+    end
+  end
+  end
+  hash
 end
+
+
+#data[:color].each do |color, array|
+ #     array.each do |name|
+  #      if name === 

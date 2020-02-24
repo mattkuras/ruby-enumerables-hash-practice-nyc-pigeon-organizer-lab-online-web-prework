4cb36a6c5ad17e7f7e6a9af5b4b749e1a2ee0b31
Matt Kuras <mattkuras@gmail.com>
2020-02-24 18:30:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,17 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-    
+      x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  binding.pry 
 end
 
 
